641e716d5630797d9e41fb148418488d7f04f429
William Ryan <billy.ryan.rom@gmail.com>
2020-02-21 08:41:42 -0500

    Add Product and Card Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,25 +19,17 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Navigation  />
+				path="/" component={Products}/>
+				component={ShoppingCart}/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+
+		</CartContext.Provider>
+	</ProductContext.Provider>
+	</div>
 	);
 }
 
